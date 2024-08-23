fbe6aaf75855db0423a0628d02dcba5a4b589c9a
Alexandria <112831738+AlexandriaPedro@users.noreply.github.com>
2024-08-23 09:30:08 +0000

    initial commit

---
+++ ex1/SomaInteiro.java
@@ -0,0 +1,13 @@
+public class SomaInteiros {
+    public static void main(String[] args) {
+        // Declara e inicializar os dois números inteiros
+        int num1 = 5;
+        int num2 = 10;
+        
+        // Soma os dois números
+        int soma = num1 + num2;
+        
+        // Exibe o resultado
+        System.out.println("A soma de " + num1 + " e " + num2 + " é: " + soma);
+    }
+}
